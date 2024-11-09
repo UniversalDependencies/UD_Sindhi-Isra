4f131861002172721e59e09cebf4aa822aec76e8
John Bauer <horatio@gmail.com>
2024-11-08 16:49:25 -0800

    Add some upos -> xpos checking for the validate script

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -4,6 +4,23 @@ import networkx as nx
 
 from stanza.utils.conll import CoNLL
 
+ALLOWED_UPOS_TO_XPOS = {
+    "NOUN":  ["NN", "NNX"],
+    "PROPN": ["NNP"],
+    "ADJ":   ["JJ", "JJC", "JJO", "JJM", "JJF"],
+    "PRON":  ["PRP", "PRD", "PRWH", "PRL"],
+    "ADV":   ["ADV", "ADT", "ADM", "ADN", "ADQ", "ADA", "ADS", "ADP", "ADPX"],
+    "ADP":   ["PSP", "PSPX"],
+    "CCONJ": ["CC"],
+    "SCONJ": ["CS"],
+    "AUX":   ["VAUX", "VAUXX", "VAUXN"],
+    "VERB":  ["VM", "VMX"],
+    "PUNCT": ["PUNCT"],
+    "PART":  ["PART"],
+    "NUM":   ["NUM"],
+    "DET":   ["PRD"],
+}
+
 def validate(new_doc, print_sent_idx=False):
     problem_sentences = set()
 
@@ -91,6 +108,19 @@ def validate(new_doc, print_sent_idx=False):
         except nx.NetworkXNoCycle:
             pass
 
+    printed = False
+    for sent_idx, sent in enumerate(new_doc.sentences):
+        for word_idx, word in enumerate(sent.words):
+            if not word.xpos or not word.upos:
+                continue
+            if word.upos in ALLOWED_UPOS_TO_XPOS:
+                if word.xpos not in ALLOWED_UPOS_TO_XPOS[word.upos]:
+                    if not printed:
+                        printed = True
+                        print("XPOS ERRORS")
+                    print("Sentence %d word %d had xpos %s which is not allowed for upos %s" % (sent_idx, word_idx, word.xpos, word.upos))
+            else:
+                print(word.upos, word.xpos)
 
     return problem_sentences
 
