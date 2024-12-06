575928820d56435b4e179b22cd3ff1ac255d7345
John Bauer <horatio@gmail.com>
2024-12-05 16:39:23 -0800

    Change Degree = Comp -> Cmp and Super -> Sup for ADJ to better match existing UD features

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -24,7 +24,7 @@ ALLOWED_UPOS_TO_XPOS = {
 }
 
 ALLOWED_UPOS_TO_FEATS = {
-    "ADJ":   ['Case=Nom', 'Case=Obl', 'Degree=Comp', 'Degree=Pos', 'Degree=Super', 'Gend=Fem', 'Gend=Masc', 'Number=Pl', 'Number=Sg'],
+    "ADJ":   ['Case=Nom', 'Case=Obl', 'Degree=Cmp', 'Degree=Pos', 'Degree=Sup', 'Gend=Fem', 'Gend=Masc', 'Number=Pl', 'Number=Sg'],
     "ADP":   ['Case=Gen', 'Case=Nom', 'Case=Obl', 'Gend=Fem', 'Gend=Masc', 'Number=Pl', 'Number=Sg', 'Person=3', 'Type=Gen', 'Type=Loc'],
     "ADV":   ['Case=Nom', 'Case=Obl', 'Gend=Fem', 'Gend=Masc', 'Number=Pl', 'Number=Sg', 'Person=3'],
     "AUX":   ['AuxType=Be', 'Gend=Fem', 'Gend=Masc', 'Mood=Subj', 'Number=Pl', 'Number=Sg', 'Person=1', 'Person=2', 'Person=3', 'Tense=Future', 'Tense=Past', 'Tense=Present'],
