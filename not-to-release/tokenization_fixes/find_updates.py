47a087278157d34130769a367c9c50c65e6fec18
John Bauer <horatio@gmail.com>
2025-03-11 08:56:18 -0700

    Update find_updates.py to also retag & reparse the sentences from one of the files

--- not-to-release/tokenization_fixes/find_updates.py
+++ not-to-release/tokenization_fixes/find_updates.py
@@ -1,5 +1,7 @@
 import difflib
 
+from stanza.utils.conll import CoNLL
+from stanza import Pipeline
 
 with open("two_nsubj.txt") as fin:
     orig_lines = fin.readlines()
@@ -15,59 +17,100 @@ print("%d lines unchanged" % unchanged)
 print("%d lines in original" % len(orig_lines))
 print("%d lines in new" % len(new_lines))
 
-orig_idx = 0
-new_idx = 0
+def yield_update_spans(orig_lines, new_lines):
+    orig_idx = 0
+    new_idx = 0
 
-while orig_idx < len(orig_lines):
-    if new_lines[new_idx] == orig_lines[orig_idx]:
-        orig_idx += 1
-        new_idx += 1
-        continue
-    matcher = difflib.SequenceMatcher(a=orig_lines[orig_idx], b=new_lines[new_idx])
-    if len(matcher.get_opcodes()) == 3:
-        operations = [x for x in matcher.get_opcodes() if x[0] != 'equal']
-        if len(operations) == 1 and operations[0][2] - operations[0][1] < 5 and operations[0][4] - operations[0][3] < 5:
+    while orig_idx < len(orig_lines):
+        if new_lines[new_idx] == orig_lines[orig_idx]:
+            yield orig_lines[orig_idx], new_lines[new_idx:new_idx+1]
             orig_idx += 1
             new_idx += 1
             continue
-    if orig_idx == 190 and new_idx == 265:
-        # difflib seems to be going haywire here, so manually skip it
-        orig_idx += 1
-        new_idx += 2
-        continue
-    for num_chunks in range(2, 8):
-        orig_line = orig_lines[orig_idx].replace(" ", "").replace(".", "")
-        new_line = "".join(new_lines[new_idx:new_idx+num_chunks]).replace(" ", "").replace(".", "")
-        if orig_line == new_line:
+        matcher = difflib.SequenceMatcher(a=orig_lines[orig_idx], b=new_lines[new_idx])
+        if len(matcher.get_opcodes()) == 3:
+            operations = [x for x in matcher.get_opcodes() if x[0] != 'equal']
+            if len(operations) == 1 and operations[0][2] - operations[0][1] < 5 and operations[0][4] - operations[0][3] < 5:
+                yield orig_lines[orig_idx], new_lines[new_idx:new_idx+1]
+                orig_idx += 1
+                new_idx += 1
+                continue
+        if orig_idx == 190 and new_idx == 265:
+            # difflib seems to be going haywire here, so manually skip it
+            yield orig_lines[orig_idx], new_lines[new_idx:new_idx+2]
+            orig_idx += 1
+            new_idx += 2
+            continue
+        for num_chunks in range(2, 8):
+            orig_line = orig_lines[orig_idx].replace(" ", "").replace(".", "")
+            new_line = "".join(new_lines[new_idx:new_idx+num_chunks]).replace(" ", "").replace(".", "")
+            if orig_line == new_line:
+                yield orig_lines[orig_idx], new_lines[new_idx:new_idx+num_chunks]
+                orig_idx += 1
+                new_idx += num_chunks
+                break
+            new_line = "".join(new_lines[new_idx:new_idx+num_chunks])
+            matcher = difflib.SequenceMatcher(a=orig_lines[orig_idx], b=new_line)
+            # expected / acceptable: equal text with a period, for example
+            if len(matcher.get_opcodes()) > num_chunks * 2 + 1:
+                continue
+            operations = [x for x in matcher.get_opcodes() if x[0] != 'equal']
+            if len(operations) > num_chunks:
+                continue
+            if any(op[4] - op[3] > 5 or op[2] - op[1] > 5 for op in operations):
+                continue
             orig_idx += 1
             new_idx += num_chunks
+            yield orig_lines[orig_idx], new_lines[new_idx:new_idx+num_chunks]
             break
-        new_line = "".join(new_lines[new_idx:new_idx+num_chunks])
-        matcher = difflib.SequenceMatcher(a=orig_lines[orig_idx], b=new_line)
-        # expected / acceptable: equal text with a period, for example
-        if len(matcher.get_opcodes()) > num_chunks * 2 + 1:
-            continue
-        operations = [x for x in matcher.get_opcodes() if x[0] != 'equal']
-        if len(operations) > num_chunks:
-            continue
-        if any(op[4] - op[3] > 5 or op[2] - op[1] > 5 for op in operations):
-            continue
-        orig_idx += 1
-        new_idx += num_chunks
-        break
-    else:
-        print("Could not figure out: %d %d" % (orig_idx, new_idx))
-        print(orig_lines[orig_idx])
-        print(new_lines[new_idx])
-        for num_chunks in range(2, 8):
-            new_line = "".join(new_lines[new_idx:new_idx+num_chunks])
+        else:
+            print("Could not figure out: %d %d" % (orig_idx, new_idx))
             print(orig_lines[orig_idx])
-            print(new_line)
+            print(new_lines[new_idx])
+            for num_chunks in range(2, 8):
+                new_line = "".join(new_lines[new_idx:new_idx+num_chunks])
+                print(orig_lines[orig_idx])
+                print(new_line)
 
-            matcher = difflib.SequenceMatcher(a=orig_lines[orig_idx], b="".join(new_lines[new_idx:new_idx+num_chunks]))
-            operations = [x for x in matcher.get_opcodes() if x[0] != 'equal']
-            print(len(matcher.get_opcodes()), num_chunks * 2 + 1, len(matcher.get_opcodes()) > num_chunks * 2 + 1)
-            print(len(operations) > num_chunks)
-            print(any(op[4] - op[3] > 5 or op[2] - op[1] > 5 for op in operations))
-            print(operations)
-        break
+                matcher = difflib.SequenceMatcher(a=orig_lines[orig_idx], b="".join(new_lines[new_idx:new_idx+num_chunks]))
+                operations = [x for x in matcher.get_opcodes() if x[0] != 'equal']
+                print(len(matcher.get_opcodes()), num_chunks * 2 + 1, len(matcher.get_opcodes()) > num_chunks * 2 + 1)
+                print(len(operations) > num_chunks)
+                print(any(op[4] - op[3] > 5 or op[2] - op[1] > 5 for op in operations))
+                print(operations)
+            break
+
+# TODO: make this an argparse
+doc = CoNLL.conll2doc("../dependencies/sd_batch_3.conllu")
+known_text = {sent.text.replace(" ", "") for sent in doc.sentences}
+
+pipe = Pipeline("sd", processors="tokenize,pos,lemma,depparse", package="default_accurate", tokenize_no_ssplit=True)
+
+docs = []
+
+errors = 0
+for span in yield_update_spans(orig_lines, new_lines):
+    if span[0].replace(" ", "") in known_text:
+        if len(span[1]) > 1 or span[0] != span[1][0]:
+            errors += 1
+            for sentence in span[1]:
+                # not efficient... not too many sentences, fortunately
+                doc = pipe(sentence)
+                assert len(doc.sentences) == 1
+                doc.sentences[0].add_comment("# orig_text = %s" % span[0])
+                for token in doc.sentences[0].tokens:
+                    token._start_char = None
+                    token._end_char = None
+                for word in doc.sentences[0].words:
+                    word._start_char = None
+                    word._end_char = None
+                doc.sentences[0].sent_id = str(len(docs)+1)
+                docs.append(doc)
+        #if len(span[1]) == 1 and span[0] != span[1][0]:
+        #    print(span[0])
+        #    print(span[1][0])
+
+print(errors)
+for doc in docs:
+    CoNLL.write_doc2conll(doc, "twonsubj_reparsed.conllu", mode='a')
+
