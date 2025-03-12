ae422f952434102cef5b892ea31c9a312c372ca2
John Bauer <horatio@gmail.com>
2025-03-12 01:00:28 -0700

    Add a small script that does the replacing of an updated batch to the dependencies and tokenization folders

---
+++ not-to-release/tokenization_fixes/replace_fixes.py
@@ -0,0 +1,52 @@
+"""
+Replace fix tokenization sentences in the tokenization dataset and the dependencies dataset
+"""
+
+import argparse
+from collections import defaultdict
+
+from stanza.utils.conll import CoNLL
+
+def replace_sentences(sentences, orig_to_new, reindex=True):
+    new_sentences = []
+    for sentence in sentences:
+        if sentence.text in orig_to_new:
+            if reindex:
+                sent_id = sentence.sent_id
+                for idx, new_sent in enumerate(orig_to_new[sentence.text]):
+                    new_sent_id = sent_id + chr(97 + idx)
+                    new_sent.sent_id = new_sent_id
+            new_sentences.extend(orig_to_new[sentence.text])
+        else:
+            new_sentences.append(sentence)
+    return new_sentences
+
+parser = argparse.ArgumentParser(description='Replace some retokenized & reparsed sentences')
+parser.add_argument('--reparsed', default="../xpos_features/sd_batch_3_retok.conllu")
+parser.add_argument('--original', default="../dependencies/sd_batch_3.conllu")
+args = parser.parse_args()
+tokenized_filename = "../tokenization/combined_tokenization.conllu"
+
+reparsed = CoNLL.conll2doc(args.reparsed)
+original = CoNLL.conll2doc(args.original)
+full_tokenized = CoNLL.conll2doc(tokenized_filename)
+
+orig_to_new = defaultdict(list)
+
+for sentence in reparsed.sentences:
+    comments = sentence.comments
+    for comment in comments:
+        if comment.startswith("# orig_text"):
+            orig_text = comment.split("=")[1].strip()
+            break
+    else:
+        raise ValueError("Missing orig_text on a sentence")
+    comments = [x for x in comments if not x.startswith("# orig_text")]
+    sentence._comments = comments
+    orig_to_new[orig_text].append(sentence)
+
+full_tokenized.sentences = replace_sentences(full_tokenized.sentences, orig_to_new)
+CoNLL.write_doc2conll(full_tokenized, tokenized_filename)
+
+original.sentences = replace_sentences(original.sentences, orig_to_new, reindex=False)
+CoNLL.write_doc2conll(original, args.original)
