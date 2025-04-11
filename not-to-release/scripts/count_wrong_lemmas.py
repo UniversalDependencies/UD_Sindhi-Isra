462a757c0703a69bb1907d4551c82a4370d541e5
John Bauer <horatio@gmail.com>
2025-04-11 16:43:53 -0700

    Add a short script (currently with no options) to count how well the predicted lemmatizer is doing

---
+++ not-to-release/scripts/count_wrong_lemmas.py
@@ -0,0 +1,39 @@
+"""
+Given a tsv of predicted lemmas and a tsv of corrected lemmas,
+report how many were correct and how many should have been the
+identity lemma.
+
+Useful for paper writing about how well the lemma prediction is working
+"""
+
+fixed_filename = "../lemmas/predicted_lemmas_b1_corrected.tsv"
+orig_filename = "../lemmas/original/predicted_lemmas_b1.tsv"
+
+with open(fixed_filename) as fin:
+    fixed_lines = fin.readlines()
+    fixed_lines = [x.strip() for x in fixed_lines[1:]]
+    fixed_lines = [x for x in fixed_lines if x]
+
+with open(orig_filename) as fin:
+    orig_lines = fin.readlines()
+    orig_lines = [x.strip() for x in orig_lines[1:]]
+    orig_lines = [x for x in orig_lines if x]
+
+assert len(orig_lines) == len(fixed_lines)
+
+total = 0
+correct = 0
+identity = 0
+
+for fl, ol in zip(fixed_lines, orig_lines):
+    pieces = fl.split("\t")
+    if len(pieces) > 3:
+        continue
+    total += 1
+    if fl == ol:
+        correct += 1
+    if pieces[0] == pieces[2]:
+        identity += 1
+
+print(correct, total)
+print(identity, total)
