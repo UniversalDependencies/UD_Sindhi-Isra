c56fe130995b555d1538706f9fba6038b9706edc
John Bauer <horatio@gmail.com>
2025-04-21 21:05:55 -0700

    Calculate kappa for UPOS

--- not-to-release/agreement/count_agreement.py
+++ not-to-release/agreement/count_agreement.py
@@ -1,4 +1,7 @@
+import sklearn
+
 from stanza.utils.conll import CoNLL
+
 
 d1 = CoNLL.conll2doc("det.p1.sarwat.output")
 d2 = CoNLL.conll2doc("det.p1.shafi.output")
@@ -8,16 +11,25 @@ agree_upos = 0
 agree_las = 0
 total_words = 0
 
+sarwat_pos = []
+shafi_pos = []
+
 assert len(d1.sentences) == len(d2.sentences)
 for s1, s2 in zip(d1.sentences, d2.sentences):
     assert len(s1.words) == len(s2.words)
 
     for w1, w2 in zip(s1.words, s2.words):
+        sarwat_pos.append(w1.upos)
+        shafi_pos.append(w2.upos)
+
         total_words += 1
         if w1.upos == w2.upos:
             agree_upos += 1
         if w1.head == w2.head and w1.deprel == w2.deprel:
             agree_las += 1
 
+kappa = sklearn.metrics.cohen_kappa_score(sarwat_pos, shafi_pos)
+
 print("UPOS", agree_upos, total_words, agree_upos / total_words)
 print("LAS", agree_las, total_words, agree_las / total_words)
+print("UPOS kappa", kappa)
