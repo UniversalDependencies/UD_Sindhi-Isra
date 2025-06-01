a98f699ff06b1783b07e52e4bc7a61d6fd83b88b
John Bauer <horatio@gmail.com>
2025-06-01 16:11:57 -0700

    This word is also always an ADP

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -89,7 +89,7 @@ for word in ('ٿيندو', 'ٿيندا', 'ٿيندي', 'ٿينديون'):
 ENFORCED_POS['هجئي'] = ["AUX"]
 ENFORCED_POS['ڪانهي'] = ["AUX"]
 ENFORCED_POS['ھئي'] = ["AUX"]
-for word in ('مطابق', 'سواءِ'):
+for word in ('مطابق', 'سواءِ', 'بنان'):
     ENFORCED_POS[word] = ['ADP']
 
 for word in ('سگهي', 'سگهنديس'):
