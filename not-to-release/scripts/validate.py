b80c2f1ceaae6d028158b45c9443b501c7a579a1
John Bauer <horatio@gmail.com>
2025-06-10 13:40:21 -0700

    Surely this is a demonstrative, not a use case for this word to be ADV

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -118,7 +118,7 @@ ENFORCED_POS['اهڙين'] = ['DET', 'ADJ']
 ENFORCED_POS['اهڙيءَ'] = ['DET', 'ADJ']
 
 # demonstratives
-demonstratives = ['ان', 'اِهو', 'انهي', 'انھي', 'انهيءِ', 'اِهو', 'اِهي', 'اھا', 'اھڙو', 'اھي', ]
+demonstratives = ['ان', 'اِهو', 'انهي', 'انھي', 'انهيءِ', 'اِهو', 'اِهي', 'اھا', 'اھڙو', 'اھي', 'ڪوبه']
 for word in demonstratives:
     ENFORCED_POS[word] = ['DET']
     ENFORCED_XPOS[word] = ['PRD']
