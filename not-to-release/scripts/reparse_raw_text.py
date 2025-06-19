2124997b8a3035f2b9f8a8681c48a95e508d88bd
John Bauer <horatio@gmail.com>
2025-06-18 17:30:43 -0700

    Add a short tool to reparse script with complete sentences

---
+++ not-to-release/scripts/reparse_raw_text.py
@@ -0,0 +1,32 @@
+from stanza import Pipeline
+from stanza.utils.conll import CoNLL
+
+pipe = Pipeline("sd", processors="tokenize,pos,lemma,depparse",
+                package="default_accurate",
+                depparse_model_path="saved_models/depparse/sd_isra_muril-large-cased_parser.pt",
+                pos_model_path="saved_models/pos/sd_isra_muril-large-cased_tagger.pt",
+                tokenize_no_ssplit=True)
+
+with open("resplit_sentences.txt") as fin:
+#with open("missing_md.2.txt") as fin:
+#with open("sd_batch_3_reparse.txt") as fin:
+    lines = fin.readlines()
+    lines = [x.strip() for x in lines]
+    lines = [x for x in lines if x]
+
+docs = [pipe(x) for x in lines]
+for doc in docs:
+    assert len(doc.sentences) == 1
+    for token in doc.sentences[0].tokens:
+        token._start_char = None
+        token._end_char = None
+    for word in doc.sentences[0].words:
+        word._start_char = None
+        word._end_char = None
+        word._lemma = None
+
+for doc in docs:
+    CoNLL.write_doc2conll(doc, "resplit.conllu", mode='a')
+    #CoNLL.write_doc2conll(doc, "missing_md.2.conllu", mode='a')
+    #CoNLL.write_doc2conll(doc, "sd_batch_3_reparse.conllu", mode='a')
+
