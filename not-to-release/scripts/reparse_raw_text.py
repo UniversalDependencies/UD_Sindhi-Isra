60da166d874414dd5f2b4bb1a7e0affcdd3d46be
John Bauer <horatio@gmail.com>
2025-07-01 22:36:36 -0700

    Add the ability to say whether or not to use the local trained models

--- not-to-release/scripts/reparse_raw_text.py
+++ not-to-release/scripts/reparse_raw_text.py
@@ -1,11 +1,24 @@
+import argparse
+
+
 from stanza import Pipeline
 from stanza.utils.conll import CoNLL
 
-pipe = Pipeline("sd", processors="tokenize,pos,lemma,depparse",
-                package="default_accurate",
-                depparse_model_path="saved_models/depparse/sd_isra_muril-large-cased_parser.pt",
-                pos_model_path="saved_models/pos/sd_isra_muril-large-cased_tagger.pt",
-                tokenize_no_ssplit=True)
+parser = argparse.ArgumentParser(description='Reparse some sentences into a conllu file')
+parser.add_argument('--use_local_models', default=False, action='store_true', help="Use models from the saved_models dirs")
+args = parser.parse_args()
+
+pipeline_args = {
+    "processors":         "tokenize,pos,lemma,depparse",
+    "package":            "default_accurate",
+    "tokenize_no_spplit": True,
+}
+
+if args.use_local_models:
+    pipeline_args["depparse_model_path"] = "saved_models/depparse/sd_isra_muril-large-cased_parser.pt"
+    pipeline_args["pos_model_path"] = "saved_models/pos/sd_isra_muril-large-cased_tagger.pt"
+
+pipe = Pipeline("sd", **pipeline_args)
 
 with open("resplit_sentences.txt") as fin:
 #with open("missing_md.2.txt") as fin:
