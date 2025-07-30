8894306ac31a19831857cab71f36484d9dc04ba1
John Bauer <horatio@gmail.com>
2025-07-30 14:11:38 -0700

    Remove the letter-tagged sent_ids as an option, not by default.  Since there are 8 sentences in sd_batch_2 which didn't have xpos added yet...

--- not-to-release/tokenization_fixes/filter_unfinished.py
+++ not-to-release/tokenization_fixes/filter_unfinished.py
@@ -1,8 +1,15 @@
-from stanza.utils.conll import CoNLL
-
+import argparse
 import glob
 
-full_tokenized = CoNLL.conll2doc("../dependencies/sd_batch_2_1000.conllu")
+from stanza.utils.conll import CoNLL
+
+parser = argparse.ArgumentParser(description='Look for sentences which have not yet been xpos-featurized')
+parser.add_argument('--ignore_split', action='store_true', default=False, dest='ignore_split',
+                    help='Ignore the sentences which are apparently already resplit, such as with a letter at the end of the sent_id')
+parser.add_argument('--upos_file', default='../dependencies/sd_batch_2_1000.conllu')
+args = parser.parse_args()
+
+full_tokenized = CoNLL.conll2doc(args.upos_file)
 retagged_ids = set()
 
 for filename in glob.glob("../xpos_features/sd_batch_2*"):
@@ -12,12 +19,15 @@ for filename in glob.glob("../xpos_features/sd_batch_2*"):
 finished = []
 unfinished = []
 
+print("Found %d sentences already retagged" % len(retagged_ids))
+
 for sentence in full_tokenized.sentences:
     try:
         if sentence.sent_id in retagged_ids:
             finished.append(sentence)
         else:
-            int(sentence.sent_id)
+            if args.ignore_split:
+                int(sentence.sent_id)
             unfinished.append(sentence)
     except ValueError:
         finished.append(sentence)
@@ -25,6 +35,7 @@ for sentence in full_tokenized.sentences:
 full_tokenized.sentences = finished
 CoNLL.write_doc2conll(full_tokenized, "sd_batch_2_1000.finished.conllu")
 
+print("Writing %d unfinished sentences" % len(unfinished))
 full_tokenized.sentences = unfinished
 CoNLL.write_doc2conll(full_tokenized, "sd_batch_2_1000.unfinished.conllu")
 
