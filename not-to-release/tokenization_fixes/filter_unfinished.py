c4bfd504e9559a378640dbaadcfc0cc28cd018e2
John Bauer <horatio@gmail.com>
2025-08-11 23:31:00 -0700

    Add an --xpos_glob feature to filter_unfinished.py

--- not-to-release/tokenization_fixes/filter_unfinished.py
+++ not-to-release/tokenization_fixes/filter_unfinished.py
@@ -6,13 +6,15 @@ from stanza.utils.conll import CoNLL
 parser = argparse.ArgumentParser(description='Look for sentences which have not yet been xpos-featurized')
 parser.add_argument('--ignore_split', action='store_true', default=False, dest='ignore_split',
                     help='Ignore the sentences which are apparently already resplit, such as with a letter at the end of the sent_id')
-parser.add_argument('--upos_file', default='../dependencies/sd_batch_2_1000.conllu')
+parser.add_argument('--upos_file', default='../dependencies/sd_nopos_1000.conllu')
+# previously would do: "../xpos_features/sd_batch_2*"
+parser.add_argument('--xpos_glob', default="../xpos_features/*")
 args = parser.parse_args()
 
 full_tokenized = CoNLL.conll2doc(args.upos_file)
 retagged_ids = set()
 
-for filename in glob.glob("../xpos_features/sd_batch_2*"):
+for filename in glob.glob(args.xpos_glob):
     already_tagged = CoNLL.conll2doc(filename)
     retagged_ids = retagged_ids.union([sent.sent_id for sent in already_tagged.sentences])
 
@@ -33,9 +35,9 @@ for sentence in full_tokenized.sentences:
         finished.append(sentence)
 
 full_tokenized.sentences = finished
-CoNLL.write_doc2conll(full_tokenized, "sd_batch_2_1000.finished.conllu")
+CoNLL.write_doc2conll(full_tokenized, "sd_nopos_1000.finished.conllu")
 
 print("Writing %d unfinished sentences" % len(unfinished))
 full_tokenized.sentences = unfinished
-CoNLL.write_doc2conll(full_tokenized, "sd_batch_2_1000.unfinished.conllu")
+CoNLL.write_doc2conll(full_tokenized, "sd_nopos_1000.unfinished.conllu")
 
