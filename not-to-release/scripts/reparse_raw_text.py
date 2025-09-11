27d7b67ef772a97f8f3638fdf57e18ebf7622847
John Bauer <horatio@gmail.com>
2025-09-11 10:52:38 -0700

    Add a flag for ssplitting the input text when reparsing raw text.  Makes the script more useful for producing new data files from raw text, rather than reparsing previously mistokenized text

--- not-to-release/scripts/reparse_raw_text.py
+++ not-to-release/scripts/reparse_raw_text.py
@@ -6,13 +6,17 @@ from stanza.utils.conll import CoNLL
 
 parser = argparse.ArgumentParser(description='Reparse some sentences into a conllu file')
 parser.add_argument('--use_local_models', default=False, action='store_true', help="Use models from the saved_models dirs")
+parser.add_argument('--ssplit', default=False, action='store_true', help='Sentence split the given text')
+parser.add_argument('--filename', default='resplit_sentences.txt', help='Which file to process')
 args = parser.parse_args()
 
 pipeline_args = {
     "processors":         "tokenize,pos,lemma,depparse",
     "package":            "default_accurate",
-    "tokenize_no_ssplit": True,
 }
+
+if not args.ssplit:
+    pipeline_args["tokenize_no_ssplit"] = True
 
 if args.use_local_models:
     pipeline_args["depparse_model_path"] = "saved_models/depparse/sd_isra_muril-large-cased_parser.pt"
@@ -20,23 +24,23 @@ if args.use_local_models:
 
 pipe = Pipeline("sd", **pipeline_args)
 
-with open("resplit_sentences.txt") as fin:
-#with open("missing_md.2.txt") as fin:
-#with open("sd_batch_3_reparse.txt") as fin:
+with open(args.filename) as fin:
     lines = fin.readlines()
     lines = [x.strip() for x in lines]
     lines = [x for x in lines if x]
 
 docs = [pipe(x) for x in lines]
 for doc in docs:
-    assert len(doc.sentences) == 1
-    for token in doc.sentences[0].tokens:
-        token._start_char = None
-        token._end_char = None
-    for word in doc.sentences[0].words:
-        word._start_char = None
-        word._end_char = None
-        word._lemma = None
+    if not args.ssplit:
+        assert len(doc.sentences) == 1
+    for sentence in doc.sentences:
+        for token in sentence.tokens:
+            token._start_char = None
+            token._end_char = None
+        for word in sentence.words:
+            word._start_char = None
+            word._end_char = None
+            word._lemma = None
 
 for doc in docs:
     CoNLL.write_doc2conll(doc, "resplit.conllu", mode='a')
