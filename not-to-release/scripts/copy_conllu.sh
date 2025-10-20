3ee0f3e741818260eb9cd8f0dad500ca1662f344
John Bauer <horatio@gmail.com>
2025-10-20 16:12:50 -0700

    Reorganize a third story into the dev set, hopefully getting over the tokens threshold for train/dev/test splits

--- not-to-release/scripts/copy_conllu.sh
+++ not-to-release/scripts/copy_conllu.sh
@@ -4,10 +4,11 @@ cat not-to-release/xpos_features/*_md_* >> sd_isra-ud-test.conllu
 cp not-to-release/xpos_features/sd_780_part_B.conllu sd_isra-ud-dev.conllu
 cat not-to-release/xpos_features/adabi_0.conllu >> sd_isra-ud-dev.conllu
 cat not-to-release/xpos_features/adabi_1.conllu >> sd_isra-ud-dev.conllu
+cat not-to-release/xpos_features/adabi_2.conllu >> sd_isra-ud-dev.conllu
 
 cp not-to-release/xpos_features/xpos_tagged_with_features.conllu sd_isra-ud-train.conllu
 
-filenames=`ls not-to-release/xpos_features/* | grep -v sd_780 | grep -v _md_ | grep -v xpos_tagged_with_features | grep -v adabi_0.conllu | grep -v adabi_1.conllu`
+filenames=`ls not-to-release/xpos_features/* | grep -v sd_780 | grep -v _md_ | grep -v xpos_tagged_with_features | grep -v adabi_0.conllu | grep -v adabi_1.conllu | grep -v adabi_2.conllu`
 for i in $filenames; do
     cat $i >> sd_isra-ud-train.conllu
 done
