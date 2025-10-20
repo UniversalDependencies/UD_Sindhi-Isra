9c7a84851c7d2ced6ab416549bc3d6eb0e85ea98
John Bauer <horatio@gmail.com>
2025-10-20 00:09:11 -0700

    Move some various updates to the proposed release dataset, moving a complete story from sindhiadabiboard.net to the dev set

--- not-to-release/scripts/copy_conllu.sh
+++ not-to-release/scripts/copy_conllu.sh
@@ -2,10 +2,11 @@ cp not-to-release/xpos_features/sd_780_part_A.conllu sd_isra-ud-test.conllu
 cat not-to-release/xpos_features/*_md_* >> sd_isra-ud-test.conllu
 
 cp not-to-release/xpos_features/sd_780_part_B.conllu sd_isra-ud-dev.conllu
+cat not-to-release/xpos_features/adabi_0.conllu >> sd_isra-ud-dev.conllu
 
 cp not-to-release/xpos_features/xpos_tagged_with_features.conllu sd_isra-ud-train.conllu
 
-filenames=`ls not-to-release/xpos_features/* | grep -v sd_780 | grep -v _md_ | grep -v xpos_tagged_with_features`
+filenames=`ls not-to-release/xpos_features/* | grep -v sd_780 | grep -v _md_ | grep -v xpos_tagged_with_features | grep -v adabi_0.conllu`
 for i in $filenames; do
     cat $i >> sd_isra-ud-train.conllu
 done
