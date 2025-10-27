dde6483df37972b0e9d670228ea09b09e0a62967
John Bauer <horatio@gmail.com>
2025-10-26 18:57:39 -0700

    Add a validation that all the cop lemmas are آهي (the official validator doesn't complain about blank)

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -713,6 +713,24 @@ def check_enforced_pos(filename, new_doc):
                                           nodes=[word_idx+1]))
     return incidents
 
+def check_cop_lemmas(filename, new_doc):
+    incidents = []
+    for sent_idx, sent in enumerate(new_doc.sentences):
+        for word_idx, word in enumerate(sent.words):
+            if word.deprel == 'cop':
+                error = None
+                if not word.lemma:
+                    error = "Sentence %s (%d) word %d (line %d) is |%s| with a deprel of cop, but a blank lemma" % (sent.sent_id, sent_idx, word.id, word.line_number+1)
+                elif word.lemma != 'آهي':
+                    error = "Sentence %s (%d) word %d (line %d) is |%s| with a deprel of cop, but a lemma of %s" % (sent.sent_id, sent_idx, word.id, word.line_number+1, word.lemma)
+                if error:
+                    incidents.append(Incident(category="cop-lemma error",
+                                              filename=filename,
+                                              sent_idx=sent_idx,
+                                              sentence=sent,
+                                              error=error,
+                                              nodes=[word_idx+1]))
+    return incidents
 
 def check_expected_features(filename, new_doc, check_feats):
     incidents = []
@@ -837,6 +855,7 @@ def validate(filename, new_doc, check_xpos=True, check_feats=True, require_xpos=
     incidents.extend(check_enforced_pos(filename, new_doc))
     incidents.extend(check_expected_features(filename, new_doc, check_feats))
     incidents.extend(check_feature_errors(filename, new_doc, check_feats))
+    incidents.extend(check_cop_lemmas(filename, new_doc))
 
     return incidents
 
