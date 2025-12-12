89be8a9ac4a79e36c9922d76bb60d1c5d4934c3f
John Bauer <horatio@gmail.com>
2025-12-12 12:20:28 -0800

    Should be able to use string reference numbers from the UD validator as well

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -1010,8 +1010,9 @@ def main():
             if incident.nodeid is not None:
                 error_nodes[sent_idx].append(incident.nodeid)
             for reference in incident.references:
-                if reference.nodeid and isinstance(reference.nodeid, int):
-                    error_nodes[sent_idx].append(reference.nodeid)
+                if reference.nodeid:
+                    # ValueError such as int(3.1) shouldn't happen since we have no enhanced
+                    error_nodes[sent_idx].append(int(reference.nodeid))
 
         for sent_idx in error_nodes:
             nodes = "highlight tokens = %s" % (" ".join("%d" % x for x in error_nodes[sent_idx]))
