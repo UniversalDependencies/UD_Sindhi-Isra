669c2a635673e30d361959a8dd0ea59e373016bf
John Bauer <horatio@gmail.com>
2026-08-26 08:05:42 -0700

    Update known features for منهنجا

--- not-to-release/scripts/validate.py
+++ not-to-release/scripts/validate.py
@@ -336,10 +336,10 @@ ENFORCED_POS_XPOS['سندم'] = [('PRON', 'PRP')]
 EXACT_FEATURES['سندم'] = ['Case=Gen', 'Number=Sing', 'Person=1', 'Poss=Yes', 'PronType=Prs']
 
 ENFORCED_POS_XPOS['منهنجو'] = [('PRON', 'PRP')]
-ENFORCED_FEATURES['منهنجو'] = ['Case=Gen', 'Number=Sing', 'Gender=Masc', 'Person=1', 'PronType=Prs']
+ENFORCED_FEATURES['منهنجو'] = ['Case=Gen', 'Gender=Masc', 'Number=Sing', 'Person=1', 'PronType=Prs']
 
 ENFORCED_POS_XPOS['منهنجا'] = [('PRON', 'PRP')]
-ENFORCED_FEATURES['منهنجا'] = ['Case=Gen', 'Number=Plur', 'Person=1', 'Gender=Masc']
+ENFORCED_FEATURES['منهنجا'] = ['Case=Gen', 'Gender=Masc', 'Number=Plur', 'Person=1', 'Poss=Yes']
 
 ENFORCED_POS_XPOS['تنهنجا'] = [('PRON', 'PRP')]
 ENFORCED_FEATURES['تنهنجا'] = ['Case=Gen', 'Number=Plur', 'Person=2']
